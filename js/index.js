95d2d4e220bddffc6e529aa9f165fed469bf0713
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-04-28 13:03:40 +0800

    Added toggle icon navbar javascript

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
 
+/*================== toggle icon navbar ==================*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nav-item');
+
+menuIcon.onclick = () => {
+      menuIcon.classList.toggle('bx-x');
+      navbar.classList.toggle('active');
+};
 
 /*================== scroll reveal ==================*/
-ScrollReveal({
-      reset: true,
-      distance: '80px',
-      duration: 2000,
-      delay: 200
-});
+// ScrollReveal({
+//       reset: true,
+//       distance: '80px',
+//       duration: 2000,
+//       delay: 200
+// });
 
-ScrollReveal().reveal('.home-content, .heading,', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+// ScrollReveal().reveal('.home-content, .heading,', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+// ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
 
 /*================== typed js ==================*/
 /*
