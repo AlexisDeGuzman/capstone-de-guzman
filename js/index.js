5116ce3debd74f73a43b5138c03a66270617b97d
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-07-26 21:18:45 +0800

    Fixed the navbar active classes when scrolling

--- js/index.js
+++ js/index.js
@@ -1,26 +1,50 @@
 /*================== Exit navbar on click ==================*/
 document.addEventListener("DOMContentLoaded", function () {
-    // Get all the navbar links
-    const navbarLinks = document.querySelectorAll("a.nav-link");
+    // Get all the section elements and header navigation links
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("header nav a");
 
-    // Add a click event listener to each link
-    navbarLinks.forEach((link) => {
-        link.addEventListener("click", function () {
-            // Remove the active class from all links
-            navbarLinks.forEach((link) => link.classList.remove("active"));
+    // Scroll event listener
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.scrollY;
 
-            // Add the active class to the clicked link
-            link.classList.add("active");
+        // Loop over each section element on the page
+        sections.forEach((sec) => {
+            const sectionTop = sec.offsetTop - 150;
+            const sectionHeight = sec.offsetHeight;
+            const sectionId = sec.getAttribute("id");
 
-            // Check if the navbar is currently expanded
-            const navbar = document.querySelector("#navbarNav");
-            const isNavbarExpanded = navbar.classList.contains("show");
+            // Check if the current scroll position is within the section's viewport
+            if (currentScroll >= sectionTop && currentScroll < sectionTop + sectionHeight) {
+                // Remove the "active" class from all navigation links
+                navLinks.forEach((link) => link.classList.remove("active"));
 
-            // If the navbar is expanded, toggle it to close
-            if (isNavbarExpanded) {
-                document.querySelector("#burger").click(); // Simulate a click on the burger icon to close the navbar
+                // Add the "active" class to the corresponding navigation link
+                document.querySelector(`header nav a[href="#${sectionId}"]`).classList.add("active");
             }
         });
+
+        // Check if the navbar is currently expanded
+        const navbar = document.querySelector("#navbarNav");
+        const isNavbarExpanded = navbar.classList.contains("show");
+
+        // If the navbar is expanded, toggle it to close
+        if (isNavbarExpanded) {
+            document.querySelector("#burger").click(); // Simulate a click on the burger icon to close the navbar
+        }
+        
+        // STICKY NAVBAR
+        const header = document.querySelector("header");
+        header.classList.toggle("sticky", currentScroll > 100);
+    });
+
+    // Click event listener for footer-iconTop
+    const footerIconTop = document.querySelector(".footer-iconTop a");
+    footerIconTop.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        // Scroll to the top of the page
+        window.scrollTo({ top: 0, behavior: "smooth" });
     });
 });
 
