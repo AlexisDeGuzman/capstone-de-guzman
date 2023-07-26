512bc303bbe9fa8ed6589c2a51d6800464c41fac
deguzmanalexisprexb <deguzmanalexisprexb@gmail.com>
2023-07-26 16:30:34 +0800

    Changed the picture of fullstack project in projects section and activate scroll reveal

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-
-/*================== toggle icon navbar ==================*/
-
-/*let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.nav-item');
-
-menuIcon.onclick = () => {
-      menuIcon.classList.toggle('bx-x');
-      navbar.classList.toggle('active');
-};*/
-
 /*================== Exit navbar on click ==================*/
 document.addEventListener("DOMContentLoaded", function () {
     // Get all the navbar links
@@ -37,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 /*================== scroll reveal ==================*/
-/*ScrollReveal({
+ScrollReveal({
       reset: false,
       distance: '10px',
       duration: 2000,
@@ -48,7 +37,7 @@ ScrollReveal().reveal('.home-content, .heading, .projects h2', { origin: 'top' }
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .box-1', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-*/
+
 
 
 /*================== typed js ==================*/
